2483827b7e76b551a117f7ec818ae52a7fe75c3f
Eshi44 <e.hameliervolino@gmail.com>
2020-05-28 16:35:47 -0400

    finished styling header

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./App.scss";
+import NumberDisplay from "../NumberDisplay/NumberDisplay";
 
 const App: React.FC = () => {
-	return <div className="App">Hello World!</div>;
+	return (
+		<div className="App">
+			<div className="Header">
+				<NumberDisplay value={0} />
+				<div className="CatFace"><span role="img" aria-label="cat face">🐱</span></div>
+				<NumberDisplay value={50} />
+			</div>
+			<div className="Body">Body</div>
+		</div>
+	);
 };
 export default App;
