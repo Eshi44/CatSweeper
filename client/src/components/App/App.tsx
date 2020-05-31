d630ee1f1983d2488ec708dc41a1986c5ab19e6f
Eshi44 <e.hameliervolino@gmail.com>
2020-05-31 12:19:12 -0400

    added a reveal yarn function

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -29,83 +29,87 @@ const App: React.FC = () => {
 	}, []);
 
 	// useEffect will open happen in running state changes
-	useEffect(()=>{
-		if(running && time <999) {
-			const timer= setInterval(()=>{
-				setTime(time +1);
+	useEffect(() => {
+		if (running && time < 999) {
+			const timer = setInterval(() => {
+				setTime(time + 1);
 			}, 1000);
 
 			return () => {
-			clearInterval(timer);
-		};
-	}
+				clearInterval(timer);
+			};
+		}
 	}, [running, time]);
 
-	useEffect(()=> {
-		if(loser) {
+	useEffect(() => {
+		if (loser) {
 			setCatFace(CatFace.lost);
 			setRunning(false);
 		}
 	}, [loser]);
 
-	const handleSquareClick = (rowParam: number, colParam: number) => (): void => {
+	const handleSquareClick = (
+		rowParam: number,
+		colParam: number
+	) => (): void => {
 		// console.log(rowParam, colParam);
 		// starting the game
-		if(!running) {
-		setRunning(true);
+		if (!running) {
+			setRunning(true);
 		}
 		const currentSquare = squares[rowParam][colParam];
 		let newSquares = squares.slice();
 
-		if (currentSquare.state === SquareState.toy || currentSquare.state === SquareState.visible) {
+		if (
+			currentSquare.state === SquareState.toy ||
+			currentSquare.state === SquareState.visible
+		) {
 			return;
 		}
 
 		if (currentSquare.value === SquareValue.yarn) {
 			// if click on yarn
 			setLoser(true);
-
+			newSquares =revealYarns();
+			setSquares(newSquares);
 		} else if (currentSquare.value === SquareValue.none) {
-
 			newSquares = openManySquares(newSquares, rowParam, colParam);
 			setSquares(newSquares);
-
 		} else {
 			newSquares[rowParam][colParam].state = SquareState.visible;
 			setSquares(newSquares);
 		}
 	};
 
-
-	const handleSquareContext = (rowParam: number, colParam: number) => (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+	const handleSquareContext = (rowParam: number, colParam: number) => (
+		e: React.MouseEvent<HTMLDivElement, MouseEvent>
+	): void => {
 		e.preventDefault();
 		// console.log("right click is working!");
 
 		// can only place a toy when game is running
-		if(!running) {
+		if (!running) {
 			return;
 		}
 
 		const currentSquares = squares.slice();
-		const currentSquare =squares[rowParam][colParam];
+		const currentSquare = squares[rowParam][colParam];
 
 		if (currentSquare.state === SquareState.visible) {
 			return;
 		} else if (currentSquare.state === SquareState.hidden) {
 			currentSquares[rowParam][colParam].state = SquareState.toy;
 			setSquares(currentSquares);
-			setYarnCounter(yarnCounter -1);
+			setYarnCounter(yarnCounter - 1);
 		} else if (currentSquare.state === SquareState.toy) {
 			currentSquares[rowParam][colParam].state = SquareState.hidden;
 			setSquares(currentSquares);
-			setYarnCounter(yarnCounter +1);
-
+			setYarnCounter(yarnCounter + 1);
 		}
-
 	};
 
 	const handleCatFaceClick = (): void => {
-		if(running) {
+		if (running) {
 			setRunning(false);
 			setTime(0);
 			setSquares(generateSquares());
@@ -121,13 +125,30 @@ const App: React.FC = () => {
 				<Button
 					key={`${rowIndex}-${colIndex}`}
 					state={square.state}
-					onClick = {handleSquareClick}
-					onContext = {handleSquareContext}
+					onClick={handleSquareClick}
+					onContext={handleSquareContext}
 					value={square.value}
 					row={rowIndex}
 					col={colIndex}
 				/>
 			))
+		);
+	};
+
+	// show yarns in lost
+	const revealYarns = (): Square[][] => {
+		const currentSquares = squares.slice();
+		// loop through each element and if its yarn make it visible
+		return currentSquares.map((row) =>
+			row.map((square) => {
+				if (square.value === SquareValue.yarn) {
+					return {
+						...square,
+						state: SquareState.visible,
+					};
+				}
+				return square;
+			})
 		);
 	};
 
